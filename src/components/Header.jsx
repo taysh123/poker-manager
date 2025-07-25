386af71d9254b458e5ccc182ec8fd5f90e36d938
tay123 <tayshofer05@gmail.com>
2025-07-25 19:16:50 +0300

    תיקון עמוד משחק קאש: הוספת טבלת חובות, שיפור עיצוב שדות קלט והתאמה לרוחב מלא, והצגת נתוני צ'יפים בטבלה.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { useNavigate, Link } from 'react-router-dom';
 import { getAuth, signOut } from 'firebase/auth';
 import './Header.css'; // ודא שקובץ ה-CSS מיובא כראוי
 
